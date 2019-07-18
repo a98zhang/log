8d78ba0743cb4997f43aeeb6c8ca2e08443ad52b
a98zhang <43802593+a98zhang@users.noreply.github.com>
2019-07-18 11:00:10 +0800

    Create linux_shell.sh

---
+++ linux_shell.sh
@@ -0,0 +1,23 @@
+# 旋转屏幕
+xrandr -o left
+xrandr -o right
+xrandr -o inverted
+xrandr -o normal
+
+# 打开文件夹
+nautilus ashle
+
+# 复制文件（层级复制）
+cp -r 
+
+# 在文件中搜索特定字符串
+grep [-acinv] [--color=auto] '搜寻字符串' filename
+
+# 设定环境变量
+export ENVIRONMENTAL_VARIABLE_EXAMPLE=JUST_AN_EXAMPLE
+
+# 查看环境便来嗯
+echo $ENVIRONMENTAL_VARIABLE_EXAMPLE
+
+# shell执行函数
+inv example-task-name --parameter-example-1 just_example_1 --parameter-example-2 just_example_2
